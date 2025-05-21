969555979f1fa77ca6568fbcbabb0f22957f7cb5
Archana Subramanian <archanait19@gmail.com>
2025-05-21 11:32:25 +0530

    Removed unwanted comments and added try with resources

--- src/main/java/com/sophophile/Main.java
+++ src/main/java/com/sophophile/Main.java
@@ -6,12 +6,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        System.out.printf("Hello and welcome!");
 
         var apiKey = System.getenv("OpenAIAPI_KEY");
         if (apiKey == null) {
@@ -38,8 +35,10 @@ public class Main {
                     .POST(HttpRequest.BodyPublishers.ofString(body))
                     .build();
 
-            var client = HttpClient.newHttpClient();
-            var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response;
+            try (var client = HttpClient.newHttpClient()) {
+                response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            }
             System.out.println("Reponse>>" + response.body());
         }
     }
